b02987faef556fd687cbecafc09f4ac4beacab67
ahlemkalech <calecheahlem@gmail.com>
2024-08-02 17:18:45 +0100

    signup login update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,23 @@ import 'tailwindcss/tailwind.css';
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import About from "./pages/About";
 import SignUp from "./components/SignUp";
-// import Login from "./components/Login";
+ import Login from "./components/Login";
+ import Update from "./components/Update";
 import ContactForm from "./pages/ContactForm";
 import AllProducts from "./comps/Allproducts";
 import AddProduct from "./comps/Addprod";
 import Errorpage from "./comps/Erorpage";
 import Cart from "./comps/Cart.jsx"
+//  import { Update } from '@mui/icons-material';
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* <Navbar /> */}
       <main className="flex-grow">
         <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/product" element={<ProductDetailsPage />} />
+          <Route path="/" element={<SignUp />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/update" element={<Update />} />
           <Route path="/contact" element={<ContactForm />} />
         </Routes>
       </main>
